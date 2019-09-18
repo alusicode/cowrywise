4ece7f9985d81e7025cfd57e6292ccf7a5bf13d6
alusicode <noeluchechukwu@gmail.com>
2019-09-18 10:13:52 +0100

    tab header

--- screens/Transactions.js
+++ screens/Transactions.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { SafeAreaView, StyleSheet } from "react-native";
 
-class Transactions extends Component {
-  state = {};
-  render() {
-    return (
+const Transactions = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
       <Container>
-        <Title>Hello world</Title>
+        <Header style={styles.bbtm}>
+          <Title>Transactions</Title>
+        </Header>
       </Container>
-    );
-  }
-}
+    </SafeAreaView>
+  );
+};
+
+Transactions.navigationOptions = {
+  header: null,
+};
+
+export default Transactions;
+
+const Header = styled.View`
+  padding: 25px 10px 0;
+  width: 100%;
+  color: #000;
+`;
+
 const Container = styled.View`
   flex: 1;
   align-items: center;
+  background-color: #f8f8ff;
 `;
-const Title = styled.Text``;
-export default Transactions;
+
+const Title = styled.Text`
+  font-weight: bold;
+  font-size: 36px;
+`;
+
+const styles = StyleSheet.create({
+  bbtm: {
+    borderBottomWidth: 1,
+    borderBottomColor: "rgba(0, 0, 0, 0.3)",
+  },
+  switchBg: {
+    backgroundColor: "#0165fc",
+  },
+  tab: {
+    width: "50%",
+  },
+});
