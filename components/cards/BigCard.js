12d067681afc55d92cb4d2393fc44ba0e618ddeb
alusicode <noeluchechukwu@gmail.com>
2019-09-20 10:44:12 +0100

    cards

---
+++ components/cards/BigCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+const CardThree = ({ title }) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Card></Card>
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  width: 100%;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+`;
+
+const Title = styled.Text`
+  color: rgba(0, 0, 0, 0.5);
+  margin-bottom: 10px;
+`;
+
+const Card = styled.View`
+  height: 200px;
+  background: #7f7fd5;
+  border-radius: 10px;
+`;
+export default CardThree;
---
+++ components/cards/index.js
@@ -0,0 +1,9 @@
+import HeadCard from "./StatsCard";
+import PlanCard from "./PlansCard";
+import CardThree from "./BigCard";
+
+module.exports = {
+  HeadCard,
+  PlanCard,
+  CardThree,
+};
